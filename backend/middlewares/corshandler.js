136f6ea99307a3c58cdf389624e40d8555a43c39
Dmitry Konychev <dalexeych@gmail.com>
2022-08-24 21:02:25 +0300

    corshandler another fix

--- backend/middlewares/corshandler.js
+++ backend/middlewares/corshandler.js
@@ -11,14 +11,15 @@ module.exports = (req, res, next) => {
   const requestHeaders = req.headers['access-control-request-headers'];
 
   if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Origin', origin);
+    res.setHeader('Access-Control-Allow-Credentials', true);
   }
 
   if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Headers', requestHeaders);
+    res.setHeader('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.setHeader('Access-Control-Allow-Headers', requestHeaders);
     res.end();
+    return;
   }
 
   next();
