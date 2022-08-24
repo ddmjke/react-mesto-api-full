8e2a5e8426c05e5fddc46ceda27d01d1df36015e
Dmitry Konychev <dalexeych@gmail.com>
2022-08-24 20:44:47 +0300

    fix: cors 2nd attempt

--- backend/middlewares/corshandler.js
+++ backend/middlewares/corshandler.js
@@ -6,7 +6,8 @@ const allowedCors = [
 const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
 module.exports = (req, res, next) => {
-  const { origin, method } = req.headers;
+  const { origin } = req.headers;
+  const { method } = req;
   const requestHeaders = req.headers['access-control-request-headers'];
 
   if (allowedCors.includes(origin)) {
